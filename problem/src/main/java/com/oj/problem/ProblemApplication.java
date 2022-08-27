896a300e71bf1d7397582bf320d2c2d41454e9e7
sphreixl <704907239@qq.com>
2022-08-27 17:51:50 +0800

    problem模块

---
+++ problem/src/main/java/com/oj/problem/ProblemApplication.java
@@ -0,0 +1,13 @@
+package com.oj.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemApplication.class, args);
+    }
+
+}
---
+++ problem/src/test/java/com/oj/problem/ProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oj.problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
