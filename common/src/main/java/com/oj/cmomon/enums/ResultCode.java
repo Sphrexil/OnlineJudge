fa79231f0b6ca5e76aa72c703addc704ca34092b
sphreixl <704907239@qq.com>
2022-08-27 20:25:49 +0800

    全局返回工具类

---
+++ common/src/main/java/com/oj/cmomon/enums/ResultCode.java
@@ -0,0 +1,63 @@
+package com.oj.cmomon.enums;
+
+public enum ResultCode {
+    /* 成功 */
+    SUCCESS(200, "成功"),
+    /* 默认失败 */
+    COMMON_FAIL(999, "失败"),
+    /* 参数错误：1000～1999 */
+    PARAM_NOT_VALID(1001, "参数无效"),
+    PARAM_IS_BLANK(1002, "参数为空"),
+    PARAM_TYPE_ERROR(1003, "参数类型错误"),
+    PARAM_NOT_COMPLETE(1004, "参数缺失"),
+    /* 用户错误 */
+    USER_NOT_LOGIN(2001, "用户未登录"),
+    USER_ACCOUNT_EXPIRED(2002, "账号已过期"),
+    USER_CREDENTIALS_ERROR(2003, "密码错误"),
+    USER_CREDENTIALS_EXPIRED(2004, "密码过期"),
+    USER_ACCOUNT_DISABLE(2005, "账号不可用"),
+    USER_ACCOUNT_LOCKED(2006, "账号被锁定"),
+    USER_ACCOUNT_NOT_EXIST(2007, "账号不存在"),
+    USER_ACCOUNT_ALREADY_EXIST(2008, "账号已存在"),
+    USER_ACCOUNT_USE_BY_OTHERS(2009, "账号下线"),
+    /* 业务错误 */
+    NO_PERMISSION(3001, "没有权限");
+    private Integer code;
+    private String msg;
+
+    ResultCode(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMessage(String message) {
+        this.msg = msg;
+    }
+
+    /**
+     * 根据code获取message
+     *
+     * @param code
+     * @return null
+     */
+    public static String getMessageByCode(Integer code) {
+        for (ResultCode ele : values()) {
+            if (ele.getCode().equals(code)) {
+                return ele.getMsg();
+            }
+        }
+        return null;
+    }
+}
---
+++ common/src/main/java/com/oj/cmomon/utils/ResponseResult.java
@@ -0,0 +1,129 @@
+package com.oj.cmomon.utils;
+
+import com.oj.cmomon.enums.ResultCode;
+
+import java.io.Serializable;
+
+public class ResponseResult<T> implements Serializable {
+
+    /**
+     * @Title: 公共返回类
+     * @Description: TODO
+     * @Author: zhenyu
+     * @DateTime: 2022/8/27 20:17
+     * @param code 状态码
+     * @param msg 状态消息
+     * @param data 携带的数据
+     */
+
+    private Integer code;
+    private String msg;
+    private T data;
+
+    /* 成功返回 */
+    public ResponseResult() {
+        this.code = ResultCode.SUCCESS.getCode();
+        this.msg = ResultCode.SUCCESS.getMsg();
+    }
+
+    public ResponseResult(Integer code, T data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public ResponseResult(Integer code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static ResponseResult okResult() {
+        ResponseResult result = new ResponseResult();
+        return result;
+    }
+
+    public static ResponseResult okResult(int code, String msg) {
+        ResponseResult result = new ResponseResult();
+        return result.ok(code, null, msg);
+    }
+
+    public static ResponseResult okResult(Object data) {
+        ResponseResult result = setResultCode(ResultCode.SUCCESS, ResultCode.SUCCESS.getMsg());
+        if (data != null) {
+            result.setData(data);
+        }
+        return result;
+    }
+
+    /* 失败返回 */
+    public static ResponseResult errorResult(int code, String msg) {
+        ResponseResult result = new ResponseResult();
+        return result.error(code, msg);
+    }
+
+    public static ResponseResult errorResult(ResultCode enums) {
+        return setResultCode(enums, enums.getMsg());
+    }
+
+    public static ResponseResult errorResult(ResultCode enums, String msg) {
+        return setResultCode(enums, msg);
+    }
+
+    /* 实体设置 */
+    public static ResponseResult setResultCode(ResultCode enums) {
+        return okResult(enums.getCode(), enums.getMsg());
+    }
+
+    private static ResponseResult setResultCode(ResultCode enums, String msg) {
+        return okResult(enums.getCode(), msg);
+    }
+
+    public ResponseResult<?> error(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+        return this;
+    }
+
+    public ResponseResult<?> ok(Integer code, T data) {
+        this.code = code;
+        this.data = data;
+        return this;
+    }
+
+    public ResponseResult<?> ok(Integer code, T data, String msg) {
+        this.code = code;
+        this.data = data;
+        this.msg = msg;
+        return this;
+    }
+
+    public ResponseResult<?> ok(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+}
