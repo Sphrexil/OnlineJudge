b2a62e251142af848ea49a71ffd147efd5287f91
Ma1one <1348869642@qq.com>
2022-09-07 19:04:28 +0800

    finish problem&test case add

--- problem/src/main/java/com/oj/controller/ProblemController.java
+++ problem/src/main/java/com/oj/controller/ProblemController.java
@@ -1,14 +1,19 @@
 package com.oj.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.oj.mojo.VO.CreateProblemVO;
 import com.oj.entity.ProblemEntity;
+import com.oj.entity.TestCaseEntity;
 import com.oj.service.ProblemService;
+import com.oj.service.TestCaseService;
 import com.oj.utils.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,10 +23,13 @@ import javax.validation.constraints.NotNull;
 @RestController
 @RequestMapping("/problem")
 @Api("Problem相关接口")
+@Slf4j
 public class ProblemController {
     @Autowired
     private ProblemService problemService;
 
+    @Autowired
+    private TestCaseService testCaseService;
     /**
      * 列表
      */
@@ -55,10 +63,22 @@ public class ProblemController {
     /**
      * 保存
      */
+//    @PostMapping("/save")
+//    public ResponseResult save(@RequestBody ProblemEntity problem){
+//		problemService.save(problem);
+//
+//        return ResponseResult.okResult();
+//    }
     @PostMapping("/save")
-    public ResponseResult save(@RequestBody ProblemEntity problem){
-		problemService.save(problem);
-
+    public ResponseResult save(@RequestBody CreateProblemVO createProblemVO){
+        ProblemEntity problem = createProblemVO.getProblem();
+        problemService.save(problem);
+        ArrayList<TestCaseEntity> cases = createProblemVO.getCases();
+        log.info("cases :{}", cases);
+        for (TestCaseEntity i : cases){
+            i.setRelatedProblem(problem.getId());
+            testCaseService.save(i);
+        }
         return ResponseResult.okResult();
     }
 
---
+++ problem/src/main/java/com/oj/mojo/VO/CreateProblemVO.java
@@ -0,0 +1,20 @@
+package com.oj.mojo.VO;
+
+import com.oj.entity.ProblemEntity;
+import com.oj.entity.TestCaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateProblemVO {
+    @NotNull
+    private ProblemEntity problem;
+    @NotNull
+    private ArrayList<TestCaseEntity> cases;
+}
