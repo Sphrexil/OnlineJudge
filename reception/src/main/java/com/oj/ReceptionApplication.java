2e67deca6ea4f2af6801f0a0871af7686b9299c3
sphreixl <704907239@qq.com>
2022-09-13 15:13:17 +0800

    对logout的swagger文档进行了优化

---
+++ reception/src/main/java/com/oj/ReceptionApplication.java
@@ -0,0 +1,13 @@
+package com.oj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceptionApplication.class, args);
+    }
+
+}
